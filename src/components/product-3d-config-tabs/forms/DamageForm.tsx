fd16add1af2ddfcff5dd5b424fe95e775196d413
hamdij0maa <hamdijomaa.x@gmail.com>
2021-01-12 10:42:06 +0100

    🚀 adding other canvas

--- src/components/product-3d-config-tabs/forms/DamageForm.tsx
+++ src/components/product-3d-config-tabs/forms/DamageForm.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import { Row, Col, Drawer } from "antd";
 import { useForm } from "react-hook-form";
 import { LazerOpenModalButton } from "./buttons/LazerOpenModalButton";
+import { DamageCanvasConfigurator } from "./canvas-configurators/DamageCanvasConfigurator";
+import { BackDamageCanvas } from "./canvas/BackDamageCanvas";
+import { FrontDamageCanvas } from "./canvas/FrontDamageCanvas";
 
 export type IDamageFormProps = {};
 
@@ -30,18 +33,26 @@ const DamageForm: React.FC<IDamageFormProps> = ({}) => {
     />
    </Row>
    <Drawer
+    height="calc(100vh - 129px)"
     placement="bottom"
     visible={drawerOpened}
     closable
     onClose={() => setDrawerOpened(false)}
    >
-    <h1>damage drawer</h1>
-    <p>
-     Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet sint,
-     repellendus pariatur consequatur delectus dolores, quidem cupiditate
-     voluptate accusamus unde magni explicabo architecto! Ipsum adipisci
-     repellat quis eos ea incidunt.
-    </p>
+    <Row justify="center" style={{ width: "100%" }}>
+     <h1>Identify Damages Area</h1>
+    </Row>
+    <Row className="container-configurator">
+     <Col span={12}>
+      <FrontDamageCanvas />
+     </Col>
+     <div className="configurator">
+      <DamageCanvasConfigurator />
+     </div>
+     <Col span={12}>
+      <BackDamageCanvas />
+     </Col>
+    </Row>
    </Drawer>
   </form>
  );
